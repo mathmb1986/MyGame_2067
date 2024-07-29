957dfe82906f705c2375cb3316f43b35a015b3b3
Mathieu Marceau Bourque <MathMB1986@hotmail.com>
2024-07-29 17:22:40 -0400

    Ajoutez des fichiers projet.

---
+++ MyGame_2067.Windows/MyGame_2067App.cs
@@ -0,0 +1,15 @@
+using Stride.Engine;
+
+namespace MyGame_2067
+{
+    class MyGame_2067App
+    {
+        static void Main(string[] args)
+        {
+            using (var game = new Game())
+            {
+                game.Run();
+            }
+        }
+    }
+}
